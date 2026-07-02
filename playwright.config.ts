c0e7778db49c31b49b0754c63b41063cfb531b62
Amiya Hidayathulla <amiyahiday208@gmail>
2026-07-02 15:17:05 +0530

    other browsers testing step in playwright made as comment

--- playwright.config.ts
+++ playwright.config.ts
@@ -38,15 +38,29 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+  //   {
+  //     name: 'firefox',
+  //     use: { ...devices['Desktop Firefox'] },
+  //   },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+  //   {
+  //     name: 'webkit',
+  //     use: { ...devices['Desktop Safari'] },
+  //   },
+  //   { 
+  //     name: 'edge', use: { ...devices['Desktop Edge'] } 
+  //   },
+
+  // // Mobile browsers
+  // { 
+  //   name: 'mobile-chrome', use: { ...devices['Pixel 5'] } 
+  // },
+  // { 
+  //   name: 'mobile-safari', use: { ...devices['iPhone 13'] } 
+  // },
+  // { 
+  //   name: 'tablet', use: { ...devices['iPad Pro 11'] } 
+  // },
 
     /* Test against mobile viewports. */
     // {
